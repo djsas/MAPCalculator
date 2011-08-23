8dd81cdb6bbd77649049114eff5c5106cea4a3d9
djsas <satoru.sasaki@charity-japan.com>
2011-08-23 13:20:33 +0900

    APの計算式を間違えていたので修正。

--- MAPCalculator.py
+++ MAPCalculator.py
@@ -6,16 +6,41 @@ MAPを計算するPythonクラス
 """
 
 class MAPCalculator:
-	results = []  #回答結果を格納する。インデックスが0から順に1位、2位、...、と代入していく
-	corrects = [] #正解とするラベルを格納する
+	def __init__(self):
+		self.clearResults()
+		self.corrects = [] #正解とするラベルを格納する
+	def clearResults(self):
+		self.results = {}  #回答結果を格納する。インデックスが0から順に1位、2位、...、と代入していく
+	def calculateAP(self, key):
+		print self.getCalculateAP(key)
+	def getCalculateAP(self, key):
+		out = ""
+		ansnum = 0.0
+		num = 0.0
+		sum = 0.0
+		last_ap = 0.0
+		for x in self.results[key]:
+			num += 1
+			if x in self.corrects:
+				ansnum += 1
+				p = ansnum / num
+				sum += p
+				last_ap = sum / num
+			out += "%s\n" % last_ap
+		return out
+	def calculateMAP(self):
+		print self.getCalculateMAP()
+	def getCalculateMAP(self):
+		for k in self.results.keys():
+			pass
 	def loadCorrects(self, f):
 		self.corrects = self.__fopen(f)
-	def loadResults(self, f):
-		self.results = self.__fopen(f)
+	def loadResults(self, key, f):
+		self.results[key] = self.__fopen(f)
 	def setCorrects(self, a):
 		self.corrects = a
-	def setResults(self, a):
-		self.results = a
+	def setResults(self, key, a):
+		self.results[key] = a
 	
 	def __fopen(self, f):
 		return [x.rstrip() for x in open(f)]
